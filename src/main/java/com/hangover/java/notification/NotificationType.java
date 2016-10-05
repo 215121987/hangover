511e91670aeb4175e06163bb29e0149267d55372
ashqures <ashqures@cisco.com>
2016-10-06 00:03:06 +0530

    Missing class

---
+++ src/main/java/com/hangover/java/notification/NotificationType.java
@@ -0,0 +1,14 @@
+package com.hangover.java.notification;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ashqures
+ * Date: 10/4/16
+ * Time: 6:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationType {
+
+    RESET_PASSWORD, REGISTRATION, OTP, ORDER, ORDER_DISPATCHED, ORDER_DELIVERED
+
+}
